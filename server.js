23442b4818d260a85b271e13fb98343b145602db
kobtair <aaqibimran0@gmail.com>
2024-06-22 20:22:19 +0500

    Initial Commit

--- server.js
+++ server.js
@@ -1,39 +1,42 @@
 const express = require('express');
-const cors = require('cors')
+const path = require('path');
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
-
-app.use(cors({origin:true,credentials: true}));
 
 // Sample database or object storing video and image target URLs by ID
 const mediaDatabase = {
   '1': {
     videoSrc: 'https://res.cloudinary.com/dqdtdhr16/video/upload/v1718889988/memo-ar-vid_zycp6q.mp4',
-    imageTargetSrc: 'https://cdn.glitch.global/ebad8ce1-2c18-42b4-93b9-42b1a1820093/kid.mind?v=1719058695406'
+    imageTargetSrc: 'https://cdn.glitch.global/ebad8ce1-2c18-42b4-93b9-42b1a1820093/targets.mind?v=1719055778456'
   },
   '2': {
-    videoSrc: 'https://res.cloudinary.com/dqdtdhr16/video/upload/v1718889988/memo-ar-vid_zycp6q.mp4',
-    imageTargetSrc: 'https://cdn.glitch.global/ebad8ce1-2c18-42b4-93b9-42b1a1820093/dino.mind?v=1719058710553'
+    videoSrc: 'https://res.cloudinary.com/dqdtdhr16/video/upload/v1718889988/another-video.mp4',
+    imageTargetSrc: 'https://cdn.glitch.global/ebad8ce1-2c18-42b4-93b9-42b1a1820093/another-target.mind'
   }
   // Add more entries as needed
 };
 
-// Endpoint to get media by ID
-app.get('/getMedia', (req, res) => {
+// Middleware
+app.use(cors());
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Endpoint to render HTML with dynamic values
+app.get('/ar-app', (req, res) => {
   const id = req.query.id;
   const media = mediaDatabase[id];
 
   if (media) {
-    res.json(media);
+    res.render('index', media);
   } else {
-    res.status(404).json({ error: 'Media not found' });
+    res.status(404).send('Media not found');
   }
 });
 
-// Serve static files if needed
-app.use(express.static('public'));
-
-// Start server
-app.listen(process.env.PORT || port,() => {
-  console.log(`Server running at http://localhost:${port}`);
+// Start server and listen on all network interfaces (0.0.0.0)
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
